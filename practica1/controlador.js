d7809cd918b9f88fae5795336d8423aeb8ca5b4e
Salomem0118 <87783709+Salomem0118@users.noreply.github.com>
2021-07-21 16:53:00 -0500

    Initial commit

---
+++ practica1/controlador.js
@@ -0,0 +1,10 @@
+//programa que nos ayude a establecer
+//el costo total de una consola de video juegos
+
+//en el exterior
+//compra en dolares
+//pagar un casillero
+//dolares consto dependiendo de la cantidad de producto 
+//envio hasta colombia
+//cantidad de productos 
+//pagar impuesto de entrada a el pais (lo digital adicional )
